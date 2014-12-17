4a20812fc8e45ac517e40aee39e96e2c6a2f6821
Eelco Hillenius <eelco.hillenius@gmail.com>
2014-12-17 12:09:01 -0500

    opened up listpage some more

--- jooq/src/main/java/com/chillenious/common/db/jooq/svc/ListPage.java
+++ jooq/src/main/java/com/chillenious/common/db/jooq/svc/ListPage.java
@@ -3,10 +3,10 @@ package com.chillenious.common.db.jooq.svc;
 import com.chillenious.common.util.Wrapped;
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.UnmodifiableIterator;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collector;
@@ -15,7 +15,7 @@ import java.util.stream.Collector;
  * A page of results in a list. Works together with {@link com.chillenious.common.db.jooq.svc.Limit}.
  * This is to wrap results of a pageable query that was requested with a Limit object.
  */
-public class ListPage<T> implements Serializable, Iterable<T>, Wrapped<ImmutableList<T>> {
+public class ListPage<T> implements Serializable, Iterable<T>, Wrapped<List<T>> {
 
     private static final long serialVersionUID = 1L;
 
@@ -23,18 +23,21 @@ public class ListPage<T> implements Serializable, Iterable<T>, Wrapped<Immutable
      * Get list page instance using the provided limit and objects.
      */
     public static <E> ListPage<E> of(Limit limit, List<E> list) {
-        return new ListPage<>(limit, ImmutableList.copyOf(list));
+        return new ListPage<>(list, limit);
     }
 
     private final Limit limit;
 
-    private final ImmutableList<T> objects;
+    private final List<T> objects;
 
     // optional field (will be null if not set) that tells whether there are more rows
     // beyond this page at the time the result is constructed
     private final Boolean hasMore;
 
-    protected ListPage(Limit limit, ImmutableList<T> objects) {
+    /**
+     * Hidden constructor used by the factory methods.
+     */
+    protected ListPage(List<T> objects, Limit limit) {
         Objects.requireNonNull(objects);
         this.limit = Objects.requireNonNull(limit);
         if (limit.getTestForMore()) {
@@ -56,6 +59,15 @@ public class ListPage<T> implements Serializable, Iterable<T>, Wrapped<Immutable
         }
     }
 
+    /**
+     * Create directly.
+     */
+    public ListPage(List<T> objects, Limit limit, Boolean hasMore) {
+        this.hasMore = hasMore;
+        this.limit = limit;
+        this.objects = objects;
+    }
+
     public int getOffset() {
         return limit.getOffset();
     }
@@ -64,12 +76,12 @@ public class ListPage<T> implements Serializable, Iterable<T>, Wrapped<Immutable
         return limit.getNumberOfRows();
     }
 
-    public ImmutableList<T> getObjects() {
+    public List<T> getObjects() {
         return objects;
     }
 
     @Override
-    public UnmodifiableIterator<T> iterator() {
+    public Iterator<T> iterator() {
         return objects.iterator();
     }
 
@@ -78,7 +90,7 @@ public class ListPage<T> implements Serializable, Iterable<T>, Wrapped<Immutable
     }
 
     @Override
-    public ImmutableList<T> getWrapped() {
+    public List<T> getWrapped() {
         return objects;
     }
 
@@ -113,7 +125,24 @@ public class ListPage<T> implements Serializable, Iterable<T>, Wrapped<Immutable
                     left.addAll(right);
                     return left;
                 },
-                (list) -> ListPage.of(limit, list),
+                (list) -> new ListPage<>(list, limit),
+                Collector.Characteristics.CONCURRENT);
+    }
+
+    /**
+     * Collector that turns stream into a list page instance using the passed in limit object.
+     *
+     * @param limit limit that was used for the query
+     */
+    public static <T> Collector<T, List<T>, ListPage<T>> toListPage(Limit limit, Boolean hasMore) {
+        return Collector.of(
+                ArrayList::new,
+                List::add,
+                (left, right) -> {
+                    left.addAll(right);
+                    return left;
+                },
+                (list) -> new ListPage<>(list, limit, hasMore),
                 Collector.Characteristics.CONCURRENT);
     }
 }
